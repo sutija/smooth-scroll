fa33fb88b0b619cc5daa11f7ff1d6df64e945494
Marko Sutija <markosutija@gmail.com>
2017-07-10 15:57:19 +0200

    init

---
+++ app.js
@@ -0,0 +1,26 @@
+$(document).ready(function () {
+  
+  $('.fake').height($('.container').height());
+  for (var i = 0; i < $('section').length; i++) {
+    $('.nav').append('<a rel="' + i + '">' + i + '</a>');
+  }
+
+  $('.nav').on('click', function (e) {
+    if ($(e.target).prop('tagName').toLowerCase() == 'a') {
+      var index = $(e.target).attr('rel');
+      var scrollTo = $('section').height() * index;
+      $('.container').css({
+        'transform': 'translate3d(0,-' + ($('section').height() * index) + 'px, 1px)'
+      });
+      $(window).scrollTop(scrollTo);
+    }
+  });
+
+  $(window).scroll(function (e) {
+    var pos = $(e.target).scrollTop();
+    $('.container').css({
+      'transform': 'translate3d(0,-' + pos + 'px, 1px)'
+    });
+  });
+
+});
